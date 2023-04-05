d2e0076f4ca5b05a97c05756edde8317905296ae
aisteron <timotheus@list.ru>
2023-04-06 01:35:54 +0300

    w06.04

--- src/assets/js/ui/index.js
+++ src/assets/js/ui/index.js
@@ -3,17 +3,22 @@ import { swiper } from "./swiper";
 import { load_yt_player, init_load_yt_player, yt_observer } from "./player";
 export async function Ui(){
 
+	masonry()
+
 	await swiper.load(qs('.header-swiper'))
 	&& (
 		swiper.init(qs('.header-swiper .swiper')),
 		swiper.init_test(qs('.swiper.test-swiper'))
 		)
 
+
 	await load_yt_player()
 	init_load_yt_player()
 	yt_observer()
 
 	hidden_contacts_footer()
+
+	
 	
 }
 
@@ -25,5 +30,27 @@ function hidden_contacts_footer(){
 			event.target.innerHTML = data
 			event.target.classList.add('opened')
 		})
+	})
+}
+
+async function masonry(){
+	await load_masonry()
+	
+	new Masonry( '#masonry', {
+		itemSelector: '.grid-item',
+  	columnWidth: 274,
+		gutter: 30
+	});
+	
+}
+
+function load_masonry(){
+	return new Promise(resolve => {
+
+		let script = document.createElement("script")
+		script.src = "/vendors/masonry.pkgd.min.js"
+		qs(".scripts-area").appendChild(script)
+
+		script.onload = () => resolve(true)
 	})
 }
